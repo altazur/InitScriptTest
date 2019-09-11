7eee72fea0881926ed535907e1c65097a2792476
altazur <altazur.91@gmail.com>
2019-09-11 22:53:00 +0300

    All done TODO: 1. Test on normal ssh client (not the free one with antibot) 2. Assertion add 3. Pytest include

--- init_script_test.py
+++ init_script_test.py
@@ -1,7 +1,13 @@
 import paramiko
 import argparse
+#import pytest
 
-init_test(server, username, password, command)
+parser = argparse.ArgumentParser(description="Test of init php script")
+parser.add_argument("--server", type=str)
+parser.add_argument("--username", type=str)
+parser.add_argument("--password", type=str)
+parser.add_argument("--command", type=str)
+args = parser.parse_args()
 
 def init_test(server, username, password, command):
 	"""
@@ -10,6 +16,12 @@ def init_test(server, username, password, command):
 		Execute command
 	"""
 	ssh = paramiko.SSHClient()
-	ssh.connect(server, username, password)
-	ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
-	assert(true)
+	ssh.load_system_host_keys()
+	ssh.connect(hostname=args.server, username=args.username, password=args.password)
+	ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(args.command, get_pty=True)
+	for line in iter(ssh_stdout.readline, ""):
+		print(line, end="")
+	print("finish")
+	ssh.close()
+
+init_test(args.server, args.username, args.password, args.command)
